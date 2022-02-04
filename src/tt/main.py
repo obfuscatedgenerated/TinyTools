2e931bcd6a3d2d3ee0f2a4eba43995f1be9e56b6
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-02-04 16:16:10 +0000

    tt fix 1

--- src/tt/main.py
+++ src/tt/main.py
@@ -1,13 +1,31 @@
 import argparse
 import os
+import asyncio
 
 ap = argparse.ArgumentParser(description="Run a tool directly to bypass PATH conflicts.")
 
 ap.add_argument(
     "tool", type=str, help="the tool you want to call"
 )
-ap.add_argument("passthrough",nargs="+", help="the arguments to pass to the tool")
+ap.add_argument("passthrough", nargs="*", help="the arguments to pass to the tool")
 
 args = ap.parse_args()
 
-os.system(" ".join([args.tool] + args.passthrough))
+async def get_PATH_tt():
+    proc = await asyncio.create_subprocess_shell(
+        "where tt", stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE
+    )
+
+    stdout, stderr = await proc.communicate()
+    if stderr:
+        if stderr.startswith(b"INFO:"):
+            print("Cannot use tt to bypass PATH conflict as TinyTools is not on the PATH.")
+            exit(1)
+        else:
+            print("Unexpected output from where: "+op)
+            exit(1)
+    else:
+        if os.path.isdir(os.path.dirname(stdout.decode().strip())):
+            os.system(os.path.join(os.path.dirname(stdout.decode().strip()),args.tool)+" "+" ".join(args.passthrough))
+
+asyncio.run(get_PATH_tt())
