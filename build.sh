5229c3ea7324974df6e0a9844ef454b98c86b1a3
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-02-05 14:56:57 +0000

    start committing spec again, just now in their own directory

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-pyinstaller --onefile ./src/tt/main.py --name=tt
-pyinstaller --onefile ./src/chars/main.py --name=chars
-pyinstaller --onefile ./src/lines/main.py --name=lines
-pyinstaller --onefile ./src/size/main.py --name=size
-pyinstaller --onefile ./src/image2ascii/main.py --name=image2ascii
+pyinstaller --onefile ./src/tt/main.py --specpath ./spec/ --name=tt
+pyinstaller --onefile ./src/chars/main.py --specpath ./spec/ --name=chars
+pyinstaller --onefile ./src/lines/main.py --specpath ./spec/ --name=lines
+pyinstaller --onefile ./src/size/main.py --specpath ./spec/ --name=size
+pyinstaller --onefile ./src/image2ascii/main.py --specpath ./spec/ --name=image2ascii
