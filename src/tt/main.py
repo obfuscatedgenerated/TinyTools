256e433b4c27a385ef3aaaab6a9f3edfa16d7f0e
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-02-05 16:16:55 +0000

    tt v2

--- src/tt/main.py
+++ src/tt/main.py
@@ -1,18 +1,24 @@
 import argparse
 import os
 import sys
+import glob
 import asyncio
+from colorama import init, Fore, Style
+init()
 
-ap = argparse.ArgumentParser(description="Run a tool directly to bypass PATH conflicts.")
+ap = argparse.ArgumentParser(description="Manage TinyTools and run a tool directly to bypass PATH conflicts.")
 
 ap.add_argument(
-    "tool", type=str, help="the tool you want to call"
+    "action", type=str, help="the action you want to call (list, run)"
 )
-ap.add_argument("passthrough", nargs="*", help="the arguments to pass to the tool")
+ap.add_argument("arguments", nargs="*", help="the arguments to pass to the action/tool")
 
 preproc = sys.argv[1:]
 
-if not (sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+if (len(preproc) < 1):
+    preproc.insert(0, "--help")
+
+if not (preproc[0] == "--help" or preproc[0] == "-h"):
     preproc.insert(0,"--")
 
 args = ap.parse_args(preproc)
@@ -25,13 +31,28 @@ async def get_PATH_tt():
     stdout, stderr = await proc.communicate()
     if stderr:
         if stderr.startswith(b"INFO:"):
-            print("Cannot use tt to bypass PATH conflict as TinyTools is not on the PATH.")
+            print("Cannot use tt as TinyTools is not on the PATH.")
             exit(1)
         else:
             print("Unexpected output from where: "+op)
             exit(1)
     else:
         if os.path.isdir(os.path.dirname(stdout.decode().strip())):
-            os.system(os.path.join(os.path.dirname(stdout.decode().strip()),args.tool)+" "+" ".join(args.passthrough))
+            return os.path.dirname(stdout.decode().strip())
 
-asyncio.run(get_PATH_tt())
+if (args.action == "list"):
+    for file in glob.glob(os.path.join(asyncio.run(get_PATH_tt())+"/*.exe")):
+        print(os.path.basename(file).replace(".exe",""))
+elif (args.action == "run"):
+    tool = args.arguments[0]
+    toolargs = args.arguments[1:]
+    toolpath = asyncio.run(get_PATH_tt())
+    for file in glob.glob(os.path.join(toolpath)+"/*.exe"):
+        if tool == os.path.basename(file).replace(".exe",""):
+            os.system(os.path.join(toolpath,file)+" "+" ".join(toolargs))
+            exit(0)
+    print("Tool not found.")
+else:
+    print(Fore.YELLOW+"Deprecation Warning: v1 syntax is being phased out! Use tt run {tool} instead of tt {tool}")
+    print(Style.RESET_ALL)
+    os.system(os.path.join(asyncio.run(get_PATH_tt()),args.action)+" "+" ".join(args.arguments))
